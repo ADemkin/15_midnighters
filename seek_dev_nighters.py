194414086393dd11981a490a7e619678a9d71dcb
antondemkin <antondemkin@yandex.ru>
2017-07-28 10:58:43 +0300

    PEP8 formatting applied.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -6,7 +6,7 @@ import datetime
 def load_attempts():
     attempts = []
     for i in range(1, 11):
-        url = 'https://devman.org/api/challenges/solution_attempts/?page='  + str(i)
+        url = 'https://devman.org/api/challenges/solution_attempts/?page=' + str(i)
         try:
             json_data = requests.get(url).json()
         except requests.exceptions.ConnectionError as error:
@@ -23,12 +23,14 @@ def get_midnighters(attempts):
         unix_timestamp = attempt["timestamp"]
         local_timezone = pytz.timezone(attempt["timezone"])
         local_time = datetime.datetime.fromtimestamp(unix_timestamp, local_timezone)
-        if local_time.hour <8:
+        if local_time.hour < 8:
             print("%s сдал работу в %s" % (attempt["username"], local_time.__format__("%H:%M")))
-  
+
+
 def main():
     print('Ищем сов...')
     get_midnighters(load_attempts())
+
 
 if __name__ == '__main__':
     main()
