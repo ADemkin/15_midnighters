bacc6898865da0dfd0cc4fdf1b7a3e9743a66f05
antondemkin <antondemkin@yandex.ru>
2017-07-29 11:18:49 +0300

    Love and hate for requests module.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,33 +4,43 @@ import datetime
 
 
 def load_attempts():
+    page_number = 1
+    number_of_pages = 1
     attempts = []
-    for page_index in range(1, 11):
+    while page_number <= number_of_pages:
         url = 'https://devman.org/api/challenges/solution_attempts/'
-        params = {"page":str(page_index)}
+        params = {"page":page_number}
         try:
             json_data = requests.get(url, params=params).json()
         except requests.exceptions.ConnectionError as error:
             print(error)
         else:
+            number_of_pages = int(json_data["number_of_pages"])
             for attempt in json_data["records"]:
                 if attempt['timestamp'] is not None:
                     attempts.append(attempt)
+        page_number += 1
     return attempts
 
 
 def get_midnighters(attempts):
+    midnighters = set()
     for attempt in attempts:
         unix_timestamp = attempt["timestamp"]
         local_timezone = pytz.timezone(attempt["timezone"])
         local_time = datetime.datetime.fromtimestamp(unix_timestamp, local_timezone)
-        if local_time.hour < 8:
-            print("%s сдал работу в %s" % (attempt["username"], local_time.__format__("%H:%M")))
+        morning_hour = 8
+        if local_time.hour < morning_hour:
+            midnighters.add(attempt["username"])
+    return midnighters
 
 
 def main():
     print('Ищем сов...')
-    get_midnighters(load_attempts())
+    midnighters = get_midnighters(load_attempts())
+    print('Вот они, наши совы:')
+    for owl in midnighters:
+        print(owl)
 
 
 if __name__ == '__main__':
